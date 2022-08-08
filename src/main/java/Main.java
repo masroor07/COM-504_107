979394245c473a1fff429932b4bd94c2cd3b7c90
masroor07 <76279044+masroor07@users.noreply.github.com>
2022-08-08 13:29:59 +0530

    Change in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("testing");
         System.out.println("Masroor");
         System.out.println("New line");
+        System.out.println("New line");
+
 
     }
 }
